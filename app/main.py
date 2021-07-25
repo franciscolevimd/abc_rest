bd3e192f63b59c52ad1a63b2a4a966bb04cbc168
Francisco Leví <francisco.levi.md@gmail.com>
2021-07-25 14:21:59 -0500

    REST API construction.

--- app/main.py
+++ app/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
 from .routes.user import user
+from .docs import tags_metadata
 
 
-app = FastAPI()
+app = FastAPI(
+	title='REST API with FastAPI & MongoDB'
+	, description='This is a simple API of the FaztCode tutorial.'
+	, version='0.0.1'
+	, openapi_tags=tags_metadata
+	)
 app.include_router(user)
--- app/routes/user.py
+++ app/routes/user.py
@@ -1,29 +1,43 @@
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Response, status
+from passlib.hash import sha256_crypt
+from bson import ObjectId
+from starlette.status import HTTP_204_NO_CONTENT
+from ..config.db import conn
+from ..schemas.user import user_entity, users_entity
+from ..models.user import User
 
 user = APIRouter()
 
 
-@user.get('/users')
+@user.get('/users', response_model=list[User], tags=['users'])
 def find_all_users():
-	return 'find_all_users'
+	return users_entity(conn.abc_db.users.find())
 
 
-@user.post('/users')
-def create_user():
-	return 'create_user'
+@user.post('/users', response_model=User, tags=['users'])
+def create_user(user:User):
+	new_user = dict(user)
+	new_user['password'] = sha256_crypt.encrypt(new_user['password'])
+	del new_user['id']
+	id = conn.abc_db.users.insert_one(new_user).inserted_id
+	user = conn.abc_db.users.find_one({'_id': id})
+	return user_entity(user)
 
 
-@user.get('/users/{user_id}')
-def find_user():
-	return 'find_user'
+@user.get('/users/{user_id}', response_model=User, tags=['users'])
+def find_user(user_id:str):
+	return user_entity(conn.abc_db.users.find_one({'_id': ObjectId(user_id)}))
 
 
-@user.put('/users/{user_id}')
-def update_user():
-	return 'update_user'
+@user.put('/users/{user_id}', response_model=User, tags=['users'])
+def update_user(user_id:str, user: User):
+	user = dict(user)
+	user['password'] = sha256_crypt.encrypt(user['password'])
+	conn.abc_db.users.find_one_and_update({'_id': ObjectId(user_id)}, {'$set': user})
+	return user_entity(conn.abc_db.users.find_one({'_id': ObjectId(user_id)}))
 
 
-@user.delete('/users/{user_id}')
-def delete_user():
-	return 'delete_user'
+@user.delete('/users/{user_id}', status_code=status.HTTP_204_NO_CONTENT, tags=['users'])
+def delete_user(user_id:str):
+	conn.abc_db.users.find_one_and_delete({'_id': ObjectId(user_id)})
+	return Response(status_code=HTTP_204_NO_CONTENT)
